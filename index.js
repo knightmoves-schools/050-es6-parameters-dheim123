1d4d350856055697c76c36ac3587e4c4843af15b
dheim123 <dheim@midland.k12.ia.us>
2025-10-27 11:57:07 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+function calcuateTotal = (a, b = 2.50) => {
+  return a + b;
+}
+
+function printTopThreeHeadlines = (...headlines) => {
+  return headlines [0] + headlines [1] + headlines [2];
+};
 
