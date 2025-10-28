b98e09ba60fff19a25fda61208090985b23ebfc3
dheim123 <dheim@midland.k12.ia.us>
2025-10-28 12:12:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-function calculateTotal = (subtotal, shipping = 2.50) => {
+const calculateTotal = (subtotal, shipping = 2.50) => {
   return subtotal + shipping;
 };
 
 function printTopThreeHeadlines = (...headlines) => {
-  return value[0] value[1] value[2];
+  return value[0] + value[1] + value[2];
 };
 
 headlines = addAll(headlines, headline0, headline1, headline2);
+
 
 
